e48da70b524037a4d96fce61515cd7217404e374
nonicalx <nonicalx@gmail.com>
2024-12-13 16:00:10 +0000

    Add constants for auth

---
+++ src/pages/auth/constants.js
@@ -0,0 +1,5 @@
+export const REDIRECT_URL = "http://localhost:5173/";
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+export const AUTHORIZE_URL = "https://accounts.spotify.com/authorize";
+export const TOKEN_URL = "https://accounts.spotify.com/token";
