dda21fff2222bcaefc86909da69962f72be3f5a0
nonicalx <nonicalx@gmail.com>
2024-12-13 16:09:25 +0000

    Add handleAuthorization fxn

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { authorize } from "./utils";
 
 export default function Auth() {
+  const handleAuthorization = (e) => {
+    e.preventDefault();
+    authorize();
+  };
   return <div>Auth</div>;
 }
