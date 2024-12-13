73fe15a675724d94aaa799bbf3fda679951b1863
nonicalx <nonicalx@gmail.com>
2024-12-13 13:54:55 +0000

    Add descriptive comment

--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,3 +1,6 @@
+/**
+ * Import all pages here to export to the AppRouter as a single import.
+ */
 import Auth from "./auth/Auth";
 import TopAlbums from "./topAlbums/TopAlbums";
 import TopArtists from "./topArtists/TopArtists";
