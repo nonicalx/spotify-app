781aad3e54d09e7735ec3b9f2e12d53757be4a92
nonicalx <nonicalx@gmail.com>
2024-12-15 21:38:31 +0000

    Add getAccessToken function

--- src/pages/auth/utils.js
+++ src/pages/auth/utils.js
@@ -1,4 +1,10 @@
-import { AUTHORIZE_URL, CLIENT_ID, REDIRECT_URL } from "./constants";
+import {
+  AUTHORIZE_URL,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL,
+  TOKEN_URL,
+} from "./constants";
 
 export const authorize = () => {
   const url = `${AUTHORIZE_URL}?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${encodeURI(
@@ -6,4 +12,17 @@ export const authorize = () => {
   )}&show_dialog=true&scope=user-read-private user-read-email`;
 
   window.location.href = url;
+};
+
+export const getAccessToken = async (code) => {
+  const body = `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURI(
+    REDIRECT_URL
+  )}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+
+  const request = new Request(TOKEN_URL, {
+    method: "POST",
+    body: body,
+  });
+
+  const response = await fetch(request);
 };
