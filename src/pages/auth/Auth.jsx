79588b5d04b564c3e972487253026635685c310b
nonicalx <nonicalx@gmail.com>
2024-12-15 21:37:51 +0000

    Call getAccessToken on load if code exist

--- src/pages/auth/Auth.jsx
+++ src/pages/auth/Auth.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { authorize } from "./utils";
+import React, { useEffect } from "react";
+import { authorize, getAccessToken } from "./utils";
+import { useSearchParams } from "react-router-dom";
 
 export default function Auth() {
+  let [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.get("code")) {
+      getAccessToken(searchParams.get("code"));
+    }
+  });
+
   const handleAuthorization = (e) => {
     e.preventDefault();
     authorize();
   };
+
   return (
     <div>
       <p>Click button to Authorize Spotify</p>
