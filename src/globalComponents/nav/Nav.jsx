223324b1fbc6ed543fbf3e321fabc893a964eb4a
nonicalx <nonicalx@gmail.com>
2024-12-17 17:20:04 +0000

    Remove useEffect for conditional navigation

--- src/globalComponents/nav/Nav.jsx
+++ src/globalComponents/nav/Nav.jsx
@@ -1,22 +1,8 @@
-import React, { useEffect } from "react";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Nav.css";
-import { isAuthenticated } from "./utils";
 import { PATHS } from "./constants";
 
 export default function Nav() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (!isAuthenticated()) {
-      navigate("/auth");
-    }
-    if (isAuthenticated() && location.pathname === "/auth") {
-      navigate(-1);
-    }
-  }, [location.pathname]);
-
   return (
     <nav>
       <ul>
