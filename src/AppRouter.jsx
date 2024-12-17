e5e64f3d014c058be9e0bf8ababadf173da26330
nonicalx <nonicalx@gmail.com>
2024-12-17 17:18:46 +0000

    Add PageLoader

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,21 +1,24 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Nav from "./globalComponents/nav/Nav";
 import Pages from "./pages/Pages.js";
+import PageLoader from "./PageLoader.jsx";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <div>
         <Nav />
-        <Routes>
-          <Route path="/" element={<Pages.Auth />} />
-          <Route path="/auth" element={<Pages.Auth />} />
-          <Route path="/top-songs" element={<Pages.TopSongs />} />
-          <Route path="/top-artists" element={<Pages.TopArtists />} />
-          <Route path="/top-albums" element={<Pages.TopAlbums />} />
-          <Route path="/my-playlists" element={<Pages.MyPlaylists />} />
-          <Route path="*" element={<Pages.NotFound />} />
-        </Routes>
+        <PageLoader>
+          <Routes>
+            <Route path="/" element={<Pages.Auth />} />
+            <Route path="/auth" element={<Pages.Auth />} />
+            <Route path="/top-songs" element={<Pages.TopSongs />} />
+            <Route path="/top-artists" element={<Pages.TopArtists />} />
+            <Route path="/top-albums" element={<Pages.TopAlbums />} />
+            <Route path="/my-playlists" element={<Pages.MyPlaylists />} />
+            <Route path="*" element={<Pages.NotFound />} />
+          </Routes>
+        </PageLoader>
       </div>
     </BrowserRouter>
   );
