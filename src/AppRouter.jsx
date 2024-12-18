6c852d1f2380acc3b79b3b19d2d91bd1f792f1f7
nonicalx <nonicalx@gmail.com>
2024-12-18 23:08:37 +0000

    Change route of /

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -10,7 +10,7 @@ export default function AppRouter() {
         <Nav />
         <PageLoader>
           <Routes>
-            <Route path="/" element={<Pages.Auth />} />
+            <Route path="/" element={<Pages.TopSongs />} />
             <Route path="/auth" element={<Pages.Auth />} />
             <Route path="/top-songs" element={<Pages.TopSongs />} />
             <Route path="/top-artists" element={<Pages.TopArtists />} />
