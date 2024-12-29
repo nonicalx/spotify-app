d9408805b693a89cded4ae79c5a3f92d526d855e
nonicalx <nonicalx@gmail.com>
2024-12-29 20:54:43 +0000

    Update isAuthenticated fxn

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
 export const isAuthenticated = () => {
-  return sessionStorage.getItem("access_token") ? true : false;
+  let isAuth = false;
+  if (sessionStorage.getItem("access_data")) {
+    const { expires_in, accessed_time } = JSON.parse(
+      sessionStorage.getItem("access_data")
+    );
+    const current_time = new Date().getTime();
+    if (current_time - accessed_time < expires_in * 1000) {
+      isAuth = true;
+    } else {
+      sessionStorage.removeItem("access_data");
+    }
+  }
+  return isAuth;
 };
