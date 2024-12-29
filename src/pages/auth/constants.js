7bad32649b34e96593ba261048efc9dbe1b4b7bd
nonicalx <nonicalx@gmail.com>
2024-12-29 20:08:12 +0000

    Update CLIENT_ID and CLIENT_SECRET value

--- src/pages/auth/constants.js
+++ src/pages/auth/constants.js
@@ -1,5 +1,5 @@
 export const REDIRECT_URL = "http://localhost:5173/";
-export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+export const CLIENT_SECRET = import.meta.env.VITE_CLIENT_SECRET;
 export const AUTHORIZE_URL = "https://accounts.spotify.com/authorize";
-export const TOKEN_URL = "https://accounts.spotify.com/token";
+export const TOKEN_URL = "https://accounts.spotify.com/api/token";
