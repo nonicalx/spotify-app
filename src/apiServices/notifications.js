5bec0ed9036ca2d823e08973910dfd476787c36f
nonicalx <nonicalx@gmail.com>
2025-01-25 22:09:19 +0000

    create notification module

---
+++ src/apiServices/notifications.js
@@ -0,0 +1,9 @@
+//Created by Nonso Oranye in 2025
+
+/**
+ * Handles notification on SUCCESS, WARNING or ERROR.
+ */
+
+export function showErrorMessage(message) {
+  return console.error(message);
+}
